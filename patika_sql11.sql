abd9dfa1ed2ba34cc080bbcd96c75318f11560b0
Medet <82754184+medet486@users.noreply.github.com>
2023-08-01 20:16:59 +0300

    Create patika_sql11.sql

---
+++ patika_sql11.sql
@@ -0,0 +1,23 @@
+--actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+(select first_name from actor
+)
+union
+(select first_name from customer
+);
+
+
+--actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(select first_name from actor
+)
+intersect
+(select first_name from customer
+);
+
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+(select first_name from actor
+)
+except
+(select first_name from customer
+);
